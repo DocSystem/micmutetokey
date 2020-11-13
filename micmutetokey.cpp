eab58e1ea476a36665f555a898aba5f8f5c9a094
DocSystem <42613513+DocSystem@users.noreply.github.com>
2020-11-13 21:21:10 +0100

    Recoded mic mute detection part

--- micmutetokey.cpp
+++ micmutetokey.cpp
@@ -1,187 +1,36 @@
 #include <windows.h>
 #include <stdio.h>
 #include <mmdeviceapi.h>
-#include <devicetopology.h>
-#include <WinUser.h>
-#include <iostream>
+#include <endpointvolume.h>
 #include <fstream>
 #include "INIReader.h"
 
-HRESULT WalkTreeBackwardsFromPart(IPart* pPart);
-HRESULT DisplayMute(IAudioMute* pMute);
+int GetMute() {
 
-int MuteStatus{ 0 };
+    HRESULT hr;
 
-int getMicrophoneMuteStatus() {
-    HRESULT hr = CoInitialize(NULL);
-    if (FAILED(hr)) {
-        printf("Failed CoInitializeEx: hr = 0x%08x\n", hr);
-        return __LINE__;
-    }
+    CoInitialize(NULL);
+    IMMDeviceEnumerator* deviceEnumerator = NULL;
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID*)&deviceEnumerator);
+    IMMDevice* defaultDevice = NULL;
 
-    // get default render endpoint
-    IMMDeviceEnumerator* pEnum = NULL;
-    hr = CoCreateInstance(
-        __uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator),
-        (void**)&pEnum
-    );
-    if (FAILED(hr)) {
-        printf("Couldn't get device enumerator: hr = 0x%08x\n", hr);
-        CoUninitialize();
-        return __LINE__;
-    }
-    IMMDevice* pDevice = NULL;
-    hr = pEnum->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
-    if (FAILED(hr)) {
-        printf("Couldn't get default render device: hr = 0x%08x\n", hr);
-        pEnum->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pEnum->Release();
+    hr = deviceEnumerator->GetDefaultAudioEndpoint(eCapture, eConsole, &defaultDevice);
+    deviceEnumerator->Release();
+    deviceEnumerator = NULL;
 
-    // get device topology object for that endpoint
-    IDeviceTopology* pDT = NULL;
-    hr = pDevice->Activate(__uuidof(IDeviceTopology), CLSCTX_ALL, NULL, (void**)&pDT);
-    if (FAILED(hr)) {
-        printf("Couldn't get device topology object: hr = 0x%08x\n", hr);
-        pDevice->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pDevice->Release();
+    IAudioEndpointVolume* endpointVolume = NULL;
+    hr = defaultDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, NULL, (LPVOID*)&endpointVolume);
+    defaultDevice->Release();
+    defaultDevice = NULL;
 
-    // get the single connector for that endpoint
-    IConnector* pConnEndpoint = NULL;
-    hr = pDT->GetConnector(0, &pConnEndpoint);
-    if (FAILED(hr)) {
-        printf("Couldn't get the connector on the endpoint: hr = 0x%08x\n", hr);
-        pDT->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pDT->Release();
+    BOOL mute;
+    hr = endpointVolume->GetMute(&mute);
 
-    // get the connector on the device that is
-    // connected to
-    // the connector on the endpoint
-    IConnector* pConnDevice = NULL;
-    hr = pConnEndpoint->GetConnectedTo(&pConnDevice);
-    if (FAILED(hr)) {
-        printf("Couldn't get the connector on the device: hr = 0x%08x\n", hr);
-        pConnEndpoint->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pConnEndpoint->Release();
-
-    // QI on the device's connector for IPart
-    IPart* pPart = NULL;
-    hr = pConnDevice->QueryInterface(__uuidof(IPart), (void**)&pPart);
-    if (FAILED(hr)) {
-        printf("Couldn't get the part: hr = 0x%08x\n", hr);
-        pConnDevice->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pConnDevice->Release();
-
-    // all the real work is done in this function
-    MuteStatus = 0;
-    hr = WalkTreeBackwardsFromPart(pPart);
-    if (FAILED(hr)) {
-        printf("Couldn't walk the tree: hr = 0x%08x\n", hr);
-        pPart->Release();
-        CoUninitialize();
-        return __LINE__;
-    }
-    pPart->Release();
-
+    endpointVolume->Release();
     CoUninitialize();
 
-    return hr;
+    return mute ? 1 : 0;
 }
-
-HRESULT WalkTreeBackwardsFromPart(IPart* pPart) {
-    HRESULT hr = S_OK;
-
-    LPWSTR pwszPartName = NULL;
-    hr = pPart->GetName(&pwszPartName);
-    //printf("Part name: %ws\n", *pwszPartName ? pwszPartName : L"(Unnamed)");
-    CoTaskMemFree(pwszPartName);
-
-    // see if this is a mute node part
-    IAudioMute* pMute = NULL;
-    hr = pPart->Activate(CLSCTX_ALL, __uuidof(IAudioMute), (void**)&pMute);
-    if (E_NOINTERFACE == hr) {
-        // not a mute node
-    }
-    else if (FAILED(hr)) {
-        printf("Unexpected failure trying to activate IAudioMute: hr = 0x%08x\n", hr);
-        return hr;
-    }
-    else {
-        // it's a mute node...
-        hr = DisplayMute(pMute);
-        if (FAILED(hr)) {
-            printf("DisplayMute failed: hr = 0x%08x", hr);
-            pMute->Release();
-            return hr;
-        }
-        if (hr == 1) {
-            MuteStatus = 1;
-        }
-        pMute->Release();
-    }
-
-    // get the list of incoming parts
-    IPartsList* pIncomingParts = NULL;
-    hr = pPart->EnumPartsIncoming(&pIncomingParts);
-    if (E_NOTFOUND == hr) {
-        // not an error... we've just reached the end of the path
-        // printf("No incoming parts at this part\n");
-        return S_OK;
-    }
-    if (FAILED(hr)) {
-        printf("Couldn't enum incoming parts: hr = 0x%08x\n", hr);
-        return hr;
-    }
-    UINT nParts = 0;
-    hr = pIncomingParts->GetCount(&nParts);
-    if (FAILED(hr)) {
-        printf("Couldn't get count of incoming parts: hr = 0x%08x\n", hr);
-        pIncomingParts->Release();
-        return hr;
-    }
-
-    // walk the tree on each incoming part recursively
-    for (UINT n = 0; n < nParts; n++) {
-        IPart* pIncomingPart = NULL;
-        hr = pIncomingParts->GetPart(n, &pIncomingPart);
-
-        hr = WalkTreeBackwardsFromPart(pIncomingPart);
-        pIncomingPart->Release();
-    }
-
-    pIncomingParts->Release();
-
-    return MuteStatus;
-}
-
-
-HRESULT DisplayMute(IAudioMute* pMute) {
-    HRESULT hr = S_OK;
-    BOOL bMuted = FALSE;
-    hr = pMute->GetMute(&bMuted);
-
-    if (FAILED(hr)) {
-        printf("GetMute failed: hr = 0x%08x\n", hr);
-        return hr;
-    }
-
-    return bMuted ? 1 : 0;
-}
-
 
 int main()
 {
@@ -191,7 +40,7 @@ int main()
         printf("Can't load 'config.ini'\n");
         std::ofstream ConfigFile("config.ini");
         ConfigFile << "[General]\n";
-        ConfigFile << "refreshRate\n\n";
+        ConfigFile << "refreshRate=100\n\n";
         ConfigFile << "[Keys]\n";
         ConfigFile << "onMute=0x82\n";
         ConfigFile << "onUnmute=0x82\n";
@@ -203,13 +52,13 @@ int main()
     int muteKeyCode = reader.GetInteger("Keys", "onMute", 0x82); // virtual key-code to simulate on mute
     int unmuteKeyCode = reader.GetInteger("Keys", "onUnmute", 0x82); // virtual key-code to simulate on unmute
     int refreshRate = 1000 / reader.GetInteger("General", "refreshRate", 100);
-    int currentMicMuteStatus = getMicrophoneMuteStatus();
+    int currentMicMuteStatus = GetMute();
     int oldCurrentMicMuteStatus = currentMicMuteStatus;
     while (true)
     {
         Sleep(refreshRate);
         oldCurrentMicMuteStatus = currentMicMuteStatus;
-        currentMicMuteStatus = getMicrophoneMuteStatus();
+        currentMicMuteStatus = GetMute();
         if (oldCurrentMicMuteStatus != currentMicMuteStatus)
         {
             if (currentMicMuteStatus == 0)
